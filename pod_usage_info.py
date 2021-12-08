0c28ea96116b22f4ea65864c7056fea7eddd4c1d
vivek <vivekjoshi@sigmoidanalytics.com>
2021-12-08 14:53:36 +0530

    use psutil instead of kubernetes module

--- pod_usage_info.py
+++ pod_usage_info.py
@@ -1,16 +1,31 @@
-from kubernetes import client, config
-from time import sleep
-
-try:
-    config.load_incluster_config()
-except config.ConfigException:
-    config.load_kube_config()
-api = client.CustomObjectsApi()
+# from kubernetes import client, config
+# from time import sleep
+import psutil
+import sched
+import time
 
 print("Hello World")
-while True:
-    resource = api.list_namespaced_custom_object(group="metrics.k8s.io", version="v1beta1", namespace="default",
-                                                 plural="pods")
-    for pod in resource["items"]:
-        print(pod['containers'], "\n")
-    sleep(10)
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def do_something(sc):
+    cpu = psutil.cpu_percent(interval=1)
+    mem = psutil.virtual_memory()
+    print(f'CPU :{cpu}')
+    print(f'Memory :{mem}')
+    s.enter(2, 1, do_something, (sc,))
+
+
+s.enter(2, 1, do_something, (s,))
+s.run()
+#
+# config.load_incluster_config()
+# api = client.CustomObjectsApi()
+#
+# while True:
+#     resource = api.list_namespaced_custom_object(group="metrics.k8s.io", version="v1beta1", namespace="default",
+#                                                  plural="pods")
+#     for pod in resource["items"]:
+#         print(pod['containers'], "\n")
+#     sleep(10)
